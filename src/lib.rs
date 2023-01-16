0a6c449f3d6bae45f93abff5c9acb6176877ae36
myssynglynx <myssynglynx@heat.tech>
2023-01-16 15:50:32 -0500

    Basic CLI implementation

---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+pub mod keygen {
+    use std::char;
+    use rand::{SeedableRng, Rng};
+    use rand_regex;
+    use rand_xorshift;
+    use regex_syntax;
+    use regex::Regex;
+
+    /// All valid formats for generated key.
+    #[derive(Debug)]
+    pub enum Format {
+        LowerCase,
+        UpperCase,
+        Numeric,
+        Alphanumeric,
+        Base64Url,
+    }
+
+    /// Input config structure.
+    pub struct Config {
+        pub length: usize,
+        pub format: Format,
+        pub seed: String,
+        pub takes: usize,
+    }
+
+    /// Implementation for config structure. Ensures only valid format is given.
+    impl Config {
+        pub fn new(length: usize, format: String, seed: String, takes: usize) -> Config {
+            let fmt = match format.to_lowercase().as_str() {
+                "lowercase" => Format::LowerCase,
+                "uppercase" => Format::UpperCase,
+                "numeric" => Format::Numeric,
+                "alphanumeric" => Format::Alphanumeric,
+                "base64url" => Format::Base64Url,
+                _ => panic!("Invalid format given. Use -h/--help to see list of valid formats.")
+            };
+
+            let len = match &fmt {
+                Format::Base64Url => 64, // Ignore length if Base64Url is specified
+                _ => length
+            };
+
+            Config {
+                length: len,
+                format: fmt,
+                seed,
+                takes,
+            }
+        }
+    }
+
+    /// Convert seed phrase into usable format.
+    fn get_seed(s: &str) -> rand_xorshift::XorShiftRng {
+        let s = if s.len() > 16 { &s[..16] }
+            else { s };
+        let mut seed_bytes = [0u8; 16];
+        seed_bytes[..s.len()].copy_from_slice(s.as_bytes());
+        rand_xorshift::XorShiftRng::from_seed(seed_bytes)
+    }
+
+    /// Get regex, formatted for deterministic keygen.
+    fn get_regex_for_seed(format: &Format, length: usize) -> String {
+        let accepted_chars = match format {
+            Format::LowerCase => "[a-z]",
+            Format::UpperCase => "[A-Z]",
+            Format::Numeric => "[\\d]",
+            Format::Alphanumeric => "[a-zA-Z\\d]",
+            Format::Base64Url => "[\\w\\d-]",
+        };
+        format!(r"{}{{{}}}", accepted_chars, length)
+    }
+
+    /// Generate deterministic random key.
+    pub fn generate_deterministic_random_key(config: &Config) -> Vec<String> {
+        let mut rng = get_seed(&config.seed);
+
+        let mut parser = regex_syntax::ParserBuilder::new().unicode(false).build();
+
+        let regex = get_regex_for_seed(&config.format, config.length);
+        let hir = parser.parse(&regex).unwrap();
+        let gen = rand_regex::Regex::with_hir(hir, 100).expect("Unable to generate random key");
+
+        (&mut rng).sample_iter(&gen).take(config.takes).collect::<Vec<String>>()
+    }
+
+    /// Get all ASCII characters from a given regex.
+    fn get_chars_from_ascii(regex: &str) -> String {
+        let mut characters: String = String::from("");
+        for i in 48..128 {
+            if let Some(c) = char::from_u32(i) {
+                if Regex::new(regex).unwrap().is_match(&c.to_string()) {
+                    characters.push(c);
+                }
+            };
+        }
+        characters
+    }
+
+    /// Get character set for a given Format.
+    fn get_charset(format: &Format) -> String {
+        let regex = match format {
+            Format::LowerCase => r"[a-z]",
+            Format::UpperCase => r"[A-Z]",
+            Format::Numeric => r"[\d]",
+            Format::Alphanumeric => r"[a-zA-Z\d]",
+            Format::Base64Url => r"[\w\d-]",
+        };
+        get_chars_from_ascii(regex)
+    }
+
+    /// Generate a random key, non-deterministically.
+    pub fn generate_random_key(config: &Config) -> Vec<String> {
+        let len = match &config.format {
+            Format::Base64Url => 64,
+            _ => config.length,
+        };
+        let mut keys = Vec::<String>::new();
+        let binding = get_charset(&config.format);
+        let charset: &[u8] = binding.as_bytes();
+
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..config.takes {
+            keys.push((0..len)
+                .map(|_| {
+                    let index = rng.gen_range(0..charset.len());
+                    charset[index] as char
+                })
+                .collect());
+        }
+        keys
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use clap::{Parser};
+use keygen::keygen;
+
+/// CLI Keygen Tool
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Length of key to generate (must be 64, 128, or 256)
+    #[arg(short, long, default_value_t = 8)]
+    length: usize,
+
+    /// Format of key to generate (uppercase, lowercase, numeric, alphanumeric, base64url)
+    #[arg(short, long, default_value = "numeric")]
+    format: String,
+
+    /// Generation seed
+    #[arg(short, long, default_value = "hello world")]
+    seed: String,
+
+    /// Deterministically generate key [defaut: false]
+    #[arg(short, long, default_value = "false")]
+    deterministic: bool,
+
+    /// Number of keys to generate
+    #[arg(short, long, default_value_t = 1)]
+    takes: usize
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let config = keygen::Config::new(args.length, args.format, args.seed, args.takes);
+
+    let keys = if args.deterministic == true {
+        println!("Deterministically generating {} random keys", args.takes);
+        keygen::generate_deterministic_random_key(&config)
+    } else {
+        println!("Generating {} random keys", args.takes);
+        keygen::generate_random_key(&config)
+    };
+
+    for key in keys {
+        println!("{}", &key);
+    }
+}
